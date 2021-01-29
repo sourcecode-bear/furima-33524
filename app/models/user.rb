6a73c3033028154e9a0494abec87ae2e0071e532
sourcecode-bear <76414122+sourcecode-bear@users.noreply.github.com>
2021-01-29 19:35:30 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         
+         VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+         validates :password, format: { with: VALID_PASSWORD_REGEX }
+         validates :email, uniqueness: true
          validates :nickname, presence: true
-         validates :email, presence: true, uniqueness: true
          validates :first_name, presence: true
          validates :last_name, presence: true
          validates :first_name_kana, presence: true
          validates :last_name_kana, presence: true
          validates :birth_date, presence: true
-end
+         validates :first_name, 
+                   :last_name, 
+                   :first_name_kana, 
+                   :last_name_kana,
+                   format: {with: /[^ -~｡-ﾟ]+/,message: 'Full-width characters'}
+        end
+
