ce339ff23d55d076f7210c62fe1a9702ad1e18d6
sourcecode-bear <76414122+sourcecode-bear@users.noreply.github.com>
2021-02-03 11:46:07 +0900

    ActiveHashの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,32 @@
 class Product < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
+   belongs_to :user
+   has_one_attached :image
+   #下記のように記述して「moduleモジュールを取り込む」
+   extend ActiveHash::Associations::ActiveRecordExtensions
+   #_idが付くモデル（クラス）と紐付ける
+   belongs_to :category
+   belongs_to :condition
+   belongs_to :cost
+   belongs_to :prefecture
+   belongs_to :days
 
-  with_options presence: true do
-   validates :title             
-   validates :text              
-   validates :category_id       
-   validates :condition_id      
-   validates :cost_id           
-   validates :prefecture_id     
-   validates :days_id           
-   validates :price             
-  end
+   #ジャンルの選択が「--」の時は保存できないようにする
+   with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :cost_id
+    validates :prefecture_id
+    validates :days_id
+   end
+   
+   with_options presence: true do
+    validates :title
+    validates :text
+    validates :category_id
+    validates :condition_id
+    validates :cost_id
+    validates :prefecture_id
+    validates :days_id
+    validates :price
+   end
 end
