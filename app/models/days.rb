e753073d3fcaf9caa162a827a29261f5e258fb57
sourcecode-bear <76414122+sourcecode-bear@users.noreply.github.com>
2021-02-04 14:22:27 +0900

    商品情報入力欄の実装

--- app/models/days.rb
+++ app/models/days.rb
@@ -1,9 +1,9 @@
 class Days < ActiveHash::Base
   self.data = [
-   { id: 1, name: '--'}
-   { id: 2, name: '１〜２日で発想'}
-   { id: 3, name: '２〜３日で発想'}
-   { id: 4, name: '４〜７日で発想'}
+   { id: 1, name: '--'},
+   { id: 2, name: '１〜２日で発想'},
+   { id: 3, name: '２〜３日で発想'},
+   { id: 4, name: '４〜７日で発想'},
   ]
 
   include ActiveHash::Associations
