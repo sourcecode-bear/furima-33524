fc007460ec1440775059fabd741607069f68e38e
sourcecode-bear <76414122+sourcecode-bear@users.noreply.github.com>
2021-02-06 19:17:34 +0900

    商品出品機能のテストコード

--- app/models/product.rb
+++ app/models/product.rb
@@ -28,6 +28,6 @@ class Product < ApplicationRecord
     validates :cost_id
     validates :prefecture_id
     validates :days_id
-    validates :price
    end
+    validates :price,numericality:{only_integer:true,greater_than_or_equal_to:300,less_than_or_equal_to:9999999}
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :product do
-    
+     title             {"test"}
+     text              {"test"}
+     category_id       {1}
+     condition_id      {1}
+     cost_id           {1}
+     prefecture_id     {1}
+     days_id           {1}
+     price             {1000}
+     after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test.image.png'), filename: 'test.image.png')
+    end
   end
 end
