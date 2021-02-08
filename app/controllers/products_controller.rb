fcc23bf2e0764962147431646eba9495a1aa86ab
sourcecode-bear <76414122+sourcecode-bear@users.noreply.github.com>
2021-02-08 15:26:16 +0900

    eachメソッドの実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except:[:index]
 
  def index
-  
+    @products = Product.all # すべてのレコードを@productsに代入
  end
 
   def new
