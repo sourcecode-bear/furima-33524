bd2eaecffc4609cf1eecf2d59b40106650a66e6a
sourcecode-bear <76414122+sourcecode-bear@users.noreply.github.com>
2021-02-10 20:18:43 +0900

    商品情報編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
-  before_action :authenticate_user!, except:[:index ,:show]
-
+  before_action :authenticate_user!, except:[:index, :show]
+  
  def index
     @products = Product.order("created_at DESC")
  end
@@ -24,9 +24,31 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+   def edit
+    @product = Product.find(params[:id])
+        redirect_to root_path if @product.user.id != current_user.id  
+    end
+  
+
+    
+
+
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+   end
+  end
+
+
  private
 
  def product_params
   params.require(:product).permit(:image, :title, :text, :category_id, :condition_id, :cost_id, :days_id, :prefecture_id, :product_id, :price).merge(user_id: current_user.id)
- end
+ end    
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'products#index'
   devise_for :users
-  resources :products, only: [:index, :new, :create, :show]
+  #patch  '/users/:id',      to: 'users#update'
+  resources :products, only: [:index, :new, :create, :show, :edit, :update]
 end
