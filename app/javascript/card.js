92efbcc65da3677937e0a9343cbc08e0f7164cfd
sourcecode-bear <76414122+sourcecode-bear@users.noreply.github.com>
2021-02-17 17:05:48 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,7 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
+  
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     
